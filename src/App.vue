af65d91196e49b22402e1bedb49565ac2d51dfb5
liuzhangyun <1760091305@qq.com>
2024-10-12 14:34:45 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-config-provider locale="zhCN">
+  <a-config-provider :locale="locale">
     <div id="app">
       <router-view></router-view>
     </div>
@@ -7,11 +7,14 @@
 </template>
 
 <script>
-
+import zhCN from 'ant-design-vue/es/locale/zh_CN'; // 中文
+import enUS from 'ant-design-vue/es/locale/en_US'; // 英文
 export default {
   name: 'App',
   data() {
-    return {}
+    return {
+      locale:zhCN
+    }
   },
 
 }
